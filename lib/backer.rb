20023414fa30128c06e78f7b2c96ce7dcc26abe1
Judy McGoldrick <jjeanius@verizon.net>
2018-01-22 05:27:11 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,14 +2,15 @@ class Backer
 
   attr_accessor :name, :project
 
+  
   def initialize(name)
     @name = name
   end
 
   def back_project(project)
-    back_project = []
-    @backed_projects << project
-    @backed_projects << self
+    @back_project << project
+    #@backed_projects << project
+    #@backed_projects << self
 
   end
 
