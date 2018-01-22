78ec7da1155c595d4fa64d0c86090ce5b766e5c1
Judy McGoldrick <jjeanius@verizon.net>
2018-01-22 05:34:07 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,12 +5,12 @@ class Backer
 
   def initialize(name)
     @name = name
-    backed_projects =[]
+    @backed_projects =[]
   end
 
   def back_project(project)
 
-    @back_project << project
+    @backed_projects << project
     #@backed_projects << project
     #@backed_projects << self
 
